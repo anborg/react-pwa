b1d916e23549241a948a1f68264babd17f1802c6
anborg <dev@gmail.com>
2020-06-30 14:19:01 -0400

    async in sWorker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,48 +1,87 @@
 const CACHE_NAME="version-1";
 
-const urlsToCache= ['index.html','offline.html', 'images/bg.webp' ]//page to show when app is offline
+const staticAssets= [
+    'index.html',
+    'offline.html',
+    'images/bg.webp'
+ ]//page to show when app is offline
 const self = this; // why alias?
 
 //install ServiceWorker - callback function after install
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)//create & open a cache db
-        .then((cache) => {
-            console.log('urlsToCache added to cache '+ CACHE_NAME);
-            return cache.addAll(urlsToCache);
-        })
-    )
+self.addEventListener('install', async (event) => {
+    const cache =  await caches.open(CACHE_NAME);
+    await cache.addAll(staticAssets);
+    return self.skipWaiting();
+    // event.waitUntil(
+    //     caches.open(CACHE_NAME)//create & open a cache db
+    //     .then((cache) => {
+    //         console.log('staticAssets added to cache '+ CACHE_NAME);
+    //         return cache.addAll(staticAssets);
+    //     })
+    // )
 
 });
 
 
 
-//Listen for FETCH requests - if something is not fetchable, assume WIFI problem, return 'offline.html'
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request) //if the request is for a cached object e.g /index.html
-        .then( () => {
-            return fetch(event.request) //just return the fetch, what if this cant be fetched? due to WIFI problem?
-                .catch(() => caches.match('offline.html'))
-        })
-    )
-
-});
 
 
 
 //Activate - remove previos caches and add new ones.
-self.addEventListener('activate', (event) => { //On 'activate'
-    const cacheWhitelist = []; // clean slate
-    cacheWhitelist.push(CACHE_NAME); //retain only cacheable items
+self.addEventListener('activate', async (event) => { //On 'activate'
+    self.clients.claim();
+// const cacheWhitelist = []; // clean slate
+    // cacheWhitelist.push(CACHE_NAME); //retain only cacheable items
 
-    event.waitUntil(
-        caches.keys().then((cacheNames) => Promise.all(
-            cacheNames.map((cacheName) => {
-                if(!cacheWhitelist.includes(cacheName)) { //keep only cache "version-1"
-                    return caches.delete(cacheName);
-                }
-            })
-        )) 
-    )
+    // event.waitUntil(
+    //     caches.keys().then((cacheNames) => Promise.all(
+    //         cacheNames.map((cacheName) => {
+    //             if(!cacheWhitelist.includes(cacheName)) { //keep only cache "version-1"
+    //                 return caches.delete(cacheName);
+    //             }
+    //         })
+    //     )) 
+    // )
 });
+
+//FOr offlien Listen for FETCH requests - if something is not fetchable, assume WIFI problem, return 'offline.html'
+self.addEventListener('fetch',  async (event) => {
+    const req =  event.request;
+    const url = new URL(req.url);
+    if(url.origin === location.origin){
+        event.respondWith(cacheFirst(req));
+    }else{
+        event.respondWith(networkAndCache(req));
+    }
+    // event.respondWith(
+    //     caches.match(event.request) //if the request is for a cached object e.g /index.html
+    //     .then( () => {
+    //         return fetch(event.request) //just return the fetch, what if this cant be fetched? due to WIFI problem?
+    //             .catch(() => caches.match('offline.html'))
+    //     })
+    // )
+
+});
+
+
+async function cacheFirst(req){
+    const cache = await caches.open(CACHE_NAME);
+    const cached = await cache.match(req);
+    if (req.cache === 'only-if-cached'){
+        return cached;//https://github.com/sveltejs/sapper-template/issues/34
+    }else{
+        return fetch(req);
+    }
+    // return cached || fetch(req);
+}
+
+async function networkAndCache(req){
+    const cache = await caches.open(CACHE_NAME);
+    try{
+        const fresh = await fetch(req);
+        await cache.put(req, fresh.clone());
+        return fresh;
+    }catch(e){
+        // return caches.match('offline.html');
+    }
+}
